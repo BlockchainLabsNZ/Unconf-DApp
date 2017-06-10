711433e0635c9e14bf0140f13a8091f44eb4ce4d
Klaus Hott Vidal <klahott@gmail.com>
2017-06-10 18:04:32 +1200

    tests for get votes

--- test/unconf.js
+++ test/unconf.js
@@ -9,14 +9,42 @@ contract("UnConf", function(accounts) {
         return crt.getMemberName(accounts[0]);
       })
       .then(function(result) {
-        assert.equal(result, "founder");
+        assert.strictEqual(result, "founder");
         return crt.addMember(accounts[1], "Isaac");
       })
       .then(function(result) {
         return crt.getMemberName(accounts[1]);
       })
       .then(function(result) {
-        assert.equal(result, "Isaac");
+        assert.strictEqual(result, "Isaac");
+        done();
+      })
+      .catch(done);
+  });
+  it("should add topics, vote and get votes", function(done) {
+    var crt;
+    UnConf.new("UnConf", { from: accounts[0] })
+      .then(function(result) {
+        crt = result;
+        return crt.newProposal("BlockChain", { from: accounts[0] });
+      })
+      .then(function(result) {
+        return crt.newProposal("Ethereum", { from: accounts[0] });
+      })
+      .then(function(result) {
+        return crt.vote(0, { from: accounts[0] });
+      })
+      .then(function(result) {
+        return crt.getTopicNameAndVotesAt(0, { from: accounts[0] });
+      })
+      .then(function(result) {
+        assert.strictEqual(result[0], "BlockChain");
+        assert.strictEqual(result[1].toNumber(), 1);
+        return crt.getTopicNameAndVotesAt(1, { from: accounts[0] });
+      })
+      .then(function(result) {
+        assert.strictEqual(result[0], "Ethereum");
+        assert.strictEqual(result[1].toNumber(), 0);
         done();
       })
       .catch(done);
